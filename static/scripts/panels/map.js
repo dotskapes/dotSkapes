2b6fa0a5c747e145b855406a0f4c3f9928250825
zack@healthscapes.org <>
2011-05-24 15:40:19 -0500

    wfs and new side panels

--- static/scripts/panels/map.js
+++ static/scripts/panels/map.js
@@ -4,6 +4,8 @@ hs.map = {
 	    var mapPanel;
 	    var thisPanel = this;
 	    var layers = {};
+	    var select = {};
+	    var control = {};
 	    if (!config)
 		config = {};
 
@@ -12,18 +14,47 @@ hs.map = {
 		if (!(node.filename in layers)) {
 		    layers[node.filename] = new OpenLayers.Layer.WMS (
 			"Map",
-			geoserver_url + '/wms',
+			geoserver_url + '/ows',
 			{
-			    layers: node.filename,
+			    layers: node.prefix + ':' + node.filename,
 			    transparent: true,
 			    format: "image/png",
 			    projection: 'EPSG:9000913',
 			},
 			{
+			    srsName: 'EPSG:900913',
 			    isBaseLayer: false,
 			    displayOutsideMaxExtent: true,
 			}
 		    );
+		    select[node.filename] = new OpenLayers.Layer.Vector("Selection", {styleMap: 
+									   new OpenLayers.Style(OpenLayers.Feature.Vector.style["select"])
+									  });
+		    control[node.filename] = new OpenLayers.Control.GetFeature({
+			protocol: OpenLayers.Protocol.WFS.fromWMSLayer(layers[node.filename], {
+			    srsName: 'EPSG:900913',
+			}),
+			/*protocol: new OpenLayers.Protocol.WFS.v1_1_0 ({
+			    url: geoserver_url + '/ows',
+			    featureType: node.filename,
+			    featureNS: node.prefix,
+			    srsName: 'EPSG:900913',
+			}),*/
+			box: true,
+			hover: false,
+			multipleKey: "shiftKey",
+			toggleKey: "ctrlKey"
+		    });
+
+		    control[node.filename].events.register("featureselected", this, function(e) {
+			select[node.filename].addFeatures([e.feature]);
+		    });
+		    control[node.filename].events.register("featureunselected", this, function(e) {
+			select[node.filename].removeFeatures([e.feature]);
+		    });
+
+		    map.addControl (control[node.filename]);
+
 		    /*layers[node.filename] = new OpenLayers.Layer.Vector (null, {
 			projection: 'EPSG:4326',
 			strategies: [new OpenLayers.Strategy.Fixed()],
@@ -34,12 +65,17 @@ hs.map = {
 			}),
 		    });*/
 		}
+		control[node.filename].activate ();
 		map.addLayer (layers[node.filename]);
+		map.addLayer (select[node.filename]);
+
 	    };
 
 	    this.removeMap = function (node) {
 		var map = mapPanel.getMap ();
 		map.removeLayer (layers[node.filename]);
+		map.removeLayer (select[node.filename]);
+		control[node.filename].deactivate ();
 	    };
 
 	    Ext.apply (config, {
--- static/scripts/panels/side.js
+++ static/scripts/panels/side.js
@@ -38,7 +38,7 @@ hs.side = {
 	    var createButton = new Ext.Button ({
 		icon: '/' + hs.application + '/static/scripts/ext/examples/shared/icons/fam/add.png',
 		handler: function (b) {
-		    var win = new hs.win.DevCreate (thisPanel);
+		    var win = new hs.win.dev.Create (thisPanel);
 		    win.show ();
 		},
 	    });
@@ -63,7 +63,7 @@ hs.side = {
 		ddGroup: 'tools',
 		listeners: {
 		    dblclick: function (node) {
-			var win = new hs.win.DevWin (node, thisPanel, true, toolPanel, {
+			var win = new hs.win.dev.Read (node, thisPanel, true, toolPanel, {
 			    width: 700,
 			    height: 500,
 			});
@@ -74,6 +74,36 @@ hs.side = {
 	    hs.side.DevPanel.superclass.constructor.call (this, data, config);
 	},
     }),
+    ToolPanel: Ext.extend (hs.tree.Panel, {
+	constructor: function (data, config) {
+	    var thisPanel = this;
+	    var toolPanel;
+
+	    if (!config)
+		config = {};
+
+	    this.setToolPanel = function (panel, tab, tn) {
+		toolPanel = {
+		    panel: panel,
+		    tabPanel: tab,
+		    toolnum: tn,
+		};
+	    };
+	    Ext.apply (config, {
+		datatype: 'tools',
+		listeners: {
+		    dblclick: function (node) {
+			var win = new hs.win.dev.Read (node, thisPanel, false, toolPanel, {
+			    width: 700,
+			    height: 500,
+			});
+			win.show ()
+		    },
+		},
+	    });
+	    hs.side.ToolPanel.superclass.constructor.call (this, data, config);
+	},
+    }),
     ResultPanel: Ext.extend (hs.tree.Panel, {
 	constructor: function (data, config) {
 	    if (!config)
@@ -82,89 +112,8 @@ hs.side = {
 	    Ext.apply (config, {
 		datatype: 'results',
 		listeners: {
-		    click: function (node, e) {
-			var perm = node.public;
-			var text = '';
-			var urlPanel;
-
-			function togglePerm  () {
-			    if (perm) {
-				text = 'Make Result Private';
-				perm = 0;
-			    }
-			    else {
-				text = 'Make Result Public';
-				perm = 1;
-			    }
-			};
-			togglePerm ();
-			
-			var result_id = node.id;
-			var title = node.filename;
-
-			var publicButton = new Ext.Button ({
-			    text: text,
-			    handler: function (b, e) {
-				Ext.Ajax.request ({
-				    method: 'GET',
-				    url: '/' + hs.application + '/tool/result/publish',
-				    params: {
-					id: result_id,
-					perm: perm,
-				    },
-				});
-				togglePerm ();
-				if (perm == 1)
-				    urlPanel.hide ();
-				else
-				    urlPanel.show ();
-				b.setText (text);
-			    },
-			});
-			
-			var result = new hs.util.IFramePanel ({
-			    region: 'center',
-			}, '/' + hs.application + '/tool/result/load?id=' + result_id);
-
-			var urlPanel = new Ext.Panel ({
-			    region: 'south',
-			    border: false,
-			    height: 'autoHeight',
-			    html: 'Public URL: ' + window.location + '/healthscapes/tool/get_result?id=' + result_id,
-			    hidden: (perm == 1),
-			});
-
-			var w = new Ext.Window ({
-			    layout: 'border',
-			    width: 800,
-			    height: 600,
-			    title: title,
-			    closeAction: 'close',
-			    items: [
-				result,
-				urlPanel,
-			    ],
-			    buttons: [
-				publicButton,
-			    ],
-			    listeners: {
-				render: function (el) {
-				    /*var panel = new Ext.Panel ({
-					html: '<iframe style="margin: 0px; padding: 0px; border: none;" width="' + (775)  + '" height="' + (500)  + '" src="/' + hs.application + '/tool/result/load?id=' + result_id + '"></iframe>',
-
-				    });
-
-				    urlPanel = new Ext.Panel ({
-					html: 'Public URL: ' + window.location + '/healthscapes/tool/get_result?id=' + result_id,
-					hidden: (perm == 1),
-				    });
-
-				    this.add (panel);
-				    this.add (urlPanel);
-				    this.doLayout ();*/
-				},
-			    },
-			});
+		    dblclick: function (node, e) {
+			var w = new hs.win.Result (node, config);
 			w.show ();
 		    },
 		},
