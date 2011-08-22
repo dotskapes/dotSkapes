d6f01c1fec7bd8ff15284675bd980dc2b892fdbc
breezer00 <breezer00@gmail.com>
2011-08-22 14:37:50 -0500

    clean css files

--- static/scripts/jquery-functions.js
+++ static/scripts/jquery-functions.js
@@ -2,39 +2,33 @@ $(document).ready(function(){
 
 	// slide down sub menu bar from head_panel
 	$('#head_panel_main').mouseover(function() {
-
 		$('#submenu').slideDown('slow', function() {
 		});
-
 	});
 
 	// hide sub menu when mouse leave
 	$('#mouseleave').mouseleave(function() {
-
 		$('#submenu').hide('slow');
-		});
+	});
 
 
 	// slide down sub menu bar from head_panel
 	$('#head_panel_docs').mouseover(function() {
-
 		$('#submenu_docs').slideDown('slow', function() {
 		});
-
 	});
 
 	// hide sub menu when mouse leave
 	$('#mouseleave').mouseleave(function() {
-
 		$('#submenu_docs').hide('slow');
-		});
+	});
 
 	// toggle display method on team/index.html
 	$("#toggle_display").click(function () {
-	$("#sortbygroup").toggle();
-	$("#sortbyname").toggle();
-	$("#listbygroup").toggle();
-	$("#listbyname").toggle();
+		$("#sortbygroup").toggle();
+		$("#sortbyname").toggle();
+		$("#listbygroup").toggle();
+		$("#listbyname").toggle();
 	});
 
 });
