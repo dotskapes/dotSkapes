2da280ed551996ef9f97598097dd2eefe98cc82e
breezer00 <breezer00@gmail.com>
2011-08-30 16:04:06 -0500

    Bug fix: drop down menu no longer piles up

--- static/scripts/jquery-functions.js
+++ static/scripts/jquery-functions.js
@@ -4,23 +4,27 @@ $(document).ready(function(){
 	$('#head_panel_main').mouseover(function() {
 		$('#submenu').slideDown('slow', function() {
 		});
+		$('#submenu_docs').slideUp('slow');
 	});
 
 	// hide sub menu when mouse leave
 	$('#mouseleave').mouseleave(function() {
-		$('#submenu').hide('slow');
+		$('#submenu').slideUp('slow');
 	});
+
 
 
 	// slide down sub menu bar from head_panel
 	$('#head_panel_docs').mouseover(function() {
 		$('#submenu_docs').slideDown('slow', function() {
 		});
+		$('#submenu').slideUp('slow');
 	});
+
 
 	// hide sub menu when mouse leave
 	$('#mouseleave').mouseleave(function() {
-		$('#submenu_docs').hide('slow');
+		$('#submenu_docs').slideUp('slow');
 	});
 
 	// toggle display method on team/index.html
