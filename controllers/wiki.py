2efe91e82ed04b2a4ed532552db741b162646402
Nico Preston <nicopresto@gmail.com>
2012-06-07 19:33:47 -0700

    Pulled out wiki models, views, controllers in preparation for transition to submodules

--- controllers/wiki.py
+++ controllers/wiki.py
@@ -1,17 +1,1 @@
-def blog():
-    return render_page ()
-
-def cases():
-    return render_page ()
-
-def tutorials():
-    return render_page ()
-
-def docs():
-    return render_page ()
-    
-def researcher():
-    return render_page()
-
-def dev():
-    return render_page()
+../submod/wiki/controllers/wiki.py
---
+++ submod/wiki/controllers/wiki.py
@@ -0,0 +1,17 @@
+def blog():
+    return render_page ()
+
+def cases():
+    return render_page ()
+
+def tutorials():
+    return render_page ()
+
+def docs():
+    return render_page ()
+
+def researcher():
+    return render_page()
+
+def dev():
+    return render_page()
